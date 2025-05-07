e3b2e952f9f3f19ae965d6acd3e575b67cea1dec
Rehan Javed <rehanjaved.official@gmail.com>
2025-05-07 17:31:16 +0500

    Make Order Screen Page

--- components/FormScreen.jsx
+++ components/FormScreen.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import {View, Text, Button, TextInput, FlatList} from 'react-native';
+import AppStyle from '../styles/app_styles';
 
 const FormScreenView = () => {
     return (
-        <View>
+        <View style={{margin: 10}}>
+            <Text style={AppStyle.text_heading}>Orders</Text>
+            <View style={AppStyle.box}>
+                <TextInput style={AppStyle.text_input} placeholder='Product Name'></TextInput>
+                <TextInput style={AppStyle.text_input} placeholder='Product Price'></TextInput>
+                <TextInput style={AppStyle.text_input} placeholder='Product Quantity'></TextInput>
+                <Button title='Add Order' color={'green'} />
+            </View>
+
             
         </View>
     );
--- styles/app_styles.jsx
+++ styles/app_styles.jsx
@@ -1,9 +1,30 @@
 import {StyleSheet} from 'react-native';
+import babelConfig from '../babel.config';
 
 const AppStyle = StyleSheet.create({
 
     text_label: {
         fontSize: 15
+    },
+
+    text_heading: {
+        fontSize: 25,
+        textAlign: 'center',
+        marginBottom: 20,
+        marginTop: 20
+    },
+
+    box: {
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: 'black',
+        padding: 10
+    },
+
+    text_input: {
+        borderBottomColor: 'black',
+        borderBottomWidth: 2,
+        marginBottom: 10
     }
 
 });
